828a60fee8ab4995ed58f07d46ce2613f2a245f8
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2020-01-28 20:39:23 +0200

    Updates

---
+++ precache-manifest.6a762c7415ef688450bb1701c017e924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb847fc9d27a00b7620080df8d566f00",
+    "url": "/robo-friends-sass/index.html"
+  },
+  {
+    "revision": "bc204f0f3880fdfd088c",
+    "url": "/robo-friends-sass/static/css/main.eec04884.chunk.css"
+  },
+  {
+    "revision": "20e30690ee891c56a2e4",
+    "url": "/robo-friends-sass/static/js/2.1c2ebb62.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robo-friends-sass/static/js/2.1c2ebb62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc204f0f3880fdfd088c",
+    "url": "/robo-friends-sass/static/js/main.e6c71895.chunk.js"
+  },
+  {
+    "revision": "80f9f3b8835d9c0c84be",
+    "url": "/robo-friends-sass/static/js/runtime-main.b613c9e7.js"
+  }
+]);
